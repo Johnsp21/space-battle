c9fac667532df2c5262ba90fb347a97af807f38c
Johnson <johnspsimeon@gmail.com>
2024-08-23 11:33:20 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,160 @@
+class USS { 
+    constructor(hull,firepower,accuracy){
+     this.hull = hull
+     this.firepower = firepower
+     this.accuracy = accuracy
+     this.method = this.method.bind(this) 
+ }
+ 
+     methodFromExample(){
+         return this.USS
+     }
+ }
+ 
+ const btnEl= document.querySelector('button')
+ 
+ btnEl.addEventListener('click',methodFromExample)
+ 
+ class Alien {
+     constructor(hull,firepower,accuracy){
+         this.hull = hull
+         this.firepower = firepower
+         this.accuracy = accuracy
+ 
+     }
+ }
+ 
+ Alien ={ship1,ship2,ship3,ship4,ship5,ship6};
+ 
+ console.log(player1);
+ console.log(hero);
+ console.log("start option");
+ //How to play //
+ 
+ 
+ document.addEventListener('keydown;event'); {
+     var event=( keyCode);{
+ 
+         // Up arrow key pressed
+         player1 -= firepowerdown;
+ 
+         // Down arrow key pressed
+         player1 += firepowerup;
+ 
+         // left arrow key pressed
+         player1 = screenLeft
+ 
+         // right key presed
+         player1 = screenRight
+ 
+     }
+ 
+     //----------[]
+     Math.floor(Math.random() * (max - min)) + min;
+    }
+    attack();{
+    console.log('Currently Targeting: ${currentTarget}');
+    return this.firepower;
+    }
+ //player1 = ("USS_Assembly", 20, 5 0.7);
+ 
+ console.log(ship1)
+ generateRandomNumber(3, 6),
+ generateRandomNumber(2, 4)
+ generateRandomNumber(0.6, 0.8);
+ 
+ let ship2 
+ generateRandomNumber(3, 6)
+ generateRandomNumber(2, 4)
+ generateRandomNumber(0.6, 0.8);
+ 
+ let ship3
+ generateRandomNumber(3, 6)
+ generateRandomNumber(2, 4)
+ generateRandomNumber(0.6, 0.8);
+ 
+ let ship4
+ generateRandomNumber(3, 6)
+ generateRandomNumber(2, 4)
+ generateRandomNumber(0.6, 0.8);
+ 
+ let ship5
+ generateRandomNumber(3, 6)
+ generateRandomNumber(2, 4)
+ generateRandomNumber(0.6, 0.8);
+ 
+ let ship6
+ generateRandomNumber(3, 6)
+ generateRandomNumber(2, 4)
+ generateRandomNumber(0.6, 0.8);
+ 
+ let clock = document.querySelector(".rndCnt");
+ clock.innerHTML = currrentRound;
+ const add_round = () => {
+     currentRound++;
+     clock.innerHTML = currentRound;
+     //clock.innerHTML = currentRound;
+ };
+ const switchAlien = () => {};
+ 
+ const initiateRound = () => {};
+ 
+ const attack = (target, firepower) => {
+ 
+ };
+ 
+ const generateAlien =(alienObj) => {
+     let new_enemy = document.createElement("img");
+     new_enemy.setAttribute(
+         "src",
+         "https://media2.giphy.com/media/wxKgOLHH01VWCRnilt/200w.gift?cid=6c09b952k421h475azjffxk9y9dg7z2040arw1oksnm541y&ep=v1_gifs_search&rid=200w.gif&ct=g"
+     );
+     new_enemy.setAttribute("class", "ufo");
+     // create alien-DOM
+     let {hull, firepower, accuracy} = alienObj;
+     let diablo = ship(hull, firepower, accuracy);
+     let battleStage = document.querySelector(".b1");
+     battleStage.append(new_enemy);
+     console.log("Alien Created: ", diablo);
+     horde.push(diablo);
+     console.log("Horde Updated!");   
+ };
+ 
+ const displayPlayer = (Hero) => {
+     let nameHolder = document.querySelector(".hero");
+     nameHolder.innerHTML = Hero;   
+ };
+ 
+ const YesOrNo = () => {
+     let oracle = document.querySelector(".survive");
+     if (p1Turn=== true){
+         oracle.innerHTML = "1";
+     }else{
+         oracle.innerHTML = "2";
+     }
+ };
+ YesOrNo()
+ const toggleTurn = () => {
+     if(p1Turn=== true){
+         p1Turn = false
+         p2Turn = true
+     }else{
+         p1Turn = true
+         p2Turn = false
+     }
+     YesOrNo()
+ }
+ 
+ 
+ const StartGame = () => {
+     //-------Start Round
+     add_round();
+     //------ displayAllplayers
+     displayPlayer("player1");
+ };
+ 
+ const battleStageconfig  = { frameWidth:20, frameHeight:20};
+ this.FontFaceSetLoadEvent('player1', 'assets/player1.png',battlestageconfig);
+ this.FontFaceSetLoadEvent('firepower', 'assets/firepower.png',battlestageconfig);
+ this.FontFaceSetLoadEvent('alien', ass)
+ 
